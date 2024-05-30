6cf27ae911b7a740ce223ea9d4141afd3875828f
iDunklet <dunkletodeus@gmail.com>
2024-05-30 19:37:36 +0200

    1

---
+++ test.py
@@ -0,0 +1,9 @@
+nome = "macaco"
+idade = "20"
+gosto_de = "python"
+print(f"Hello World  {nome} veio {idade}")
+
+nome = "diferente"
+
+print(f"Hello World {nome} world")
+print(f"Hello World.{gosto_de} cabocle")
