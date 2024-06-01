d6a224421293222f6b0c9716766f39616d202609
iDunklet <dunkletodeus@gmail.com>
2024-06-01 13:36:28 +0200

    2

--- test.py
+++ test.py
@@ -1,6 +1,10 @@
 nome = "macaco"
 idade = "20"
 gosto_de = "python"
+numero = 10 
+numero_quebrado = 1.8 
+is_male = True / False #macaco
+
 print(f"Hello World  {nome} veio {idade}")
 
 nome = "diferente"
---
+++ test2.py
@@ -0,0 +1,27 @@
+num1 = 5
+num2 = 2.5
+
+a = float(num1)
+b = int(num2)
+
+print(10 // 3)
+
+print(10 % 5)
+
+print(5 ** 2)
+
+print(abs(-5))
+print(pow(2,5))
+print(max(1,10,20,40))
+print(min(a,10,20,40))
+print(round(3.5))
+
+import math
+
+print(math.floor(3.9))
+print(math.ceil(3.9))
+print(math.sqrt(9))
+
+
+
+
